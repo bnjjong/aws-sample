c48ad566f9ad26a96fd897a4cd0ccae84145a455
bnjjong <dogfootmaster@gmail.com>
2020-07-08 17:35:45 +0900

    로그인 기능 추가 함.

--- src/main/java/com/aws/sample/springboot/web/IndexController.java
+++ src/main/java/com/aws/sample/springboot/web/IndexController.java
@@ -8,8 +8,10 @@
 
 package com.aws.sample.springboot.web;
 
+import com.aws.sample.springboot.domain.config.auth.dto.SessionUser;
 import com.aws.sample.springboot.service.PostsService;
 import com.aws.sample.springboot.web.dto.PostsResponseDto;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,10 +32,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
   private final PostsService postsService;
+  private final HttpSession httpSession;
 
   @GetMapping("/")
   public String index(Model model) {
     model.addAttribute("posts", postsService.findAllDesc());
+
+    SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    if(user != null) {
+      model.addAttribute("userName", user.getName());
+    }
+
     // 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정
     // index 를 반환 하므로 src/main/resources/templates/index.mustache 전환 됨.
     return "index";
